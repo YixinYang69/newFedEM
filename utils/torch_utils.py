075c9bbc712ce5c2eb6f6fe640170c68fe7c01d2
YixinYang69 <107279730+YixinYang69@users.noreply.github.com>
2023-07-11 07:49:05 -0400

    Update torch_utils.py

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import itertools
+from collections import defaultdict
 
 
 def krum_learners(learners, target_learner, f):
@@ -115,6 +116,242 @@ def average_learners(
                 state_dict = learner.model.state_dict()
                 target_state_dict[key].data += state_dict[key].data.clone()
 
+def byzantine_robust_aggregate_tm(
+        learners,
+        target_learner,
+        weights=None,
+        average_params=True,
+        average_gradients=False,
+        beta=0.05):
+    """
+    Compute the trimmed mean of a list of learners_ensemble and store it into learner
+
+    :param learners:
+    :type learners: List[Learner]
+    :param target_learner:
+    :type target_learner: Learner
+    :param weights: tensor of the same size as learners_ensemble, having values between 0 and 1, and summing to 1,
+                    if None, uniform learners_weights are used
+    :param average_params: if set to true the parameters are averaged; default is True
+    :param average_gradients: if set to true the gradient are also averaged; default is False
+    :type weights: torch.Tensor
+
+    """
+    if not average_params and not average_gradients:
+        return
+
+    target_state_dict = target_learner.model.state_dict(keep_vars=True)
+    
+    param_val = defaultdict(list)
+    grad_val = defaultdict(list)
+
+    for key in target_state_dict:
+
+        if target_state_dict[key].data.dtype == torch.float32:
+
+            if average_params:
+                target_state_dict[key].data.fill_(0.)
+
+            if average_gradients:
+                target_state_dict[key].grad = target_state_dict[key].data.clone()
+                target_state_dict[key].grad.data.fill_(0.)
+
+            for learner_id, learner in enumerate(learners):
+                state_dict = learner.model.state_dict(keep_vars=True)
+
+                if average_params:
+                    # target_state_dict[key].data += weights[learner_id] * state_dict[key].data.clone()
+                    param_val[key].append(state_dict[key].data.clone())
+
+                if average_gradients:
+                    if state_dict[key].grad is not None:
+                        # target_state_dict[key].grad += weights[learner_id] * state_dict[key].grad.clone()
+                        grad_val[key].append(state_dict[key].grad.clone())
+                    elif state_dict[key].requires_grad:
+                        warnings.warn(
+                            "trying to average_gradients before back propagation,"
+                            " you should set `average_gradients=False`."
+                        )
+
+            N_removed = int(beta*len(learners))
+            if average_params:
+                sorted_params, _ = torch.sort(torch.stack(param_val[key], dim=0), dim=0)
+                target_state_dict[key].data = torch.mean(sorted_params[N_removed:-N_removed], dim=0)
+            
+            if average_gradients:
+                sorted_grads, _ = torch.sort(torch.stack(grad_val[key], dim=0), dim=0)
+                target_state_dict[key].grad = torch.mean(sorted_grads[N_removed:-N_removed], dim=0)
+
+        else:
+            # tracked batches
+            target_state_dict[key].data.fill_(0)
+            for learner_id, learner in enumerate(learners):
+                state_dict = learner.model.state_dict()
+                target_state_dict[key].data += state_dict[key].data.clone()
+
+def byzantine_robust_aggregate_median(
+        learners,
+        target_learner,
+        weights=None,
+        average_params=True,
+        average_gradients=False):
+    """
+    Compute the median of a list of learners_ensemble and store it into learner
+
+    :param learners:
+    :type learners: List[Learner]
+    :param target_learner:
+    :type target_learner: Learner
+    :param weights: tensor of the same size as learners_ensemble, having values between 0 and 1, and summing to 1,
+                    if None, uniform learners_weights are used
+    :param average_params: if set to true the parameters are averaged; default is True
+    :param average_gradients: if set to true the gradient are also averaged; default is False
+    :type weights: torch.Tensor
+
+    """
+    if not average_params and not average_gradients:
+        return
+
+    # if weights is None:
+    #     n_learners = len(learners)
+    #     weights = (1 / n_learners) * torch.ones(n_learners, device=learners[0].device)
+
+    # else:
+    #     weights = weights.to(learners[0].device)
+
+    target_state_dict = target_learner.model.state_dict(keep_vars=True)
+    
+    param_val = defaultdict(list)
+    grad_val = defaultdict(list)
+
+    for key in target_state_dict:
+
+        if target_state_dict[key].data.dtype == torch.float32:
+
+            if average_params:
+                target_state_dict[key].data.fill_(0.)
+
+            if average_gradients:
+                target_state_dict[key].grad = target_state_dict[key].data.clone()
+                target_state_dict[key].grad.data.fill_(0.)
+
+            for learner_id, learner in enumerate(learners):
+                state_dict = learner.model.state_dict(keep_vars=True)
+
+                if average_params:
+                    # target_state_dict[key].data += weights[learner_id] * state_dict[key].data.clone()
+                    param_val[key].append(state_dict[key].data.clone())
+
+                if average_gradients:
+                    if state_dict[key].grad is not None:
+                        # target_state_dict[key].grad += weights[learner_id] * state_dict[key].grad.clone()
+                        grad_val[key].append(state_dict[key].grad.clone())
+                    elif state_dict[key].requires_grad:
+                        warnings.warn(
+                            "trying to average_gradients before back propagation,"
+                            " you should set `average_gradients=False`."
+                        )
+
+            if average_params:
+                target_state_dict[key].data, _ = torch.median(torch.stack(param_val[key], dim=0), dim=0)
+            
+            if average_gradients:
+                target_state_dict[key].grad, _ = torch.median(torch.stack(grad_val[key], dim=0), dim=0)
+
+        else:
+            # tracked batches
+            target_state_dict[key].data.fill_(0)
+            for learner_id, learner in enumerate(learners):
+                state_dict = learner.model.state_dict()
+                target_state_dict[key].data += state_dict[key].data.clone()
+                
+def krum_agg(params, f=1):
+    """
+    Compute the krum aggregation.
+    """
+    N = len(params) # num of clients
+    k = N - f - 2
+
+    params_copy = torch.stack(params, dim=0)
+    params_ = torch.clone(params_copy).reshape(N,-1)
+    l2_dist = torch.cdist(params_, params_, p=2.0)
+
+    k_closest_dist, _ = torch.topk(l2_dist, k=k+1, dim=1, largest=False)
+    krum_idx = torch.argmin(k_closest_dist.sum(dim=1))
+    return params[krum_idx]
+
+def byzantine_robust_aggregate_krum(
+        learners,
+        target_learner,
+        weights=None,
+        average_params=True,
+        average_gradients=False):
+    """
+    Compute the krum of a list of learners_ensemble and store it into learner
+
+    :param learners:
+    :type learners: List[Learner]
+    :param target_learner:
+    :type target_learner: Learner
+    :param weights: tensor of the same size as learners_ensemble, having values between 0 and 1, and summing to 1,
+                    if None, uniform learners_weights are used
+    :param average_params: if set to true the parameters are averaged; default is True
+    :param average_gradients: if set to true the gradient are also averaged; default is False
+    :type weights: torch.Tensor
+
+    """
+    if not average_params and not average_gradients:
+        return
+
+    target_state_dict = target_learner.model.state_dict(keep_vars=True)
+    
+    param_val = defaultdict(list)
+    grad_val = defaultdict(list)
+
+    for key in target_state_dict:
+
+        if target_state_dict[key].data.dtype == torch.float32:
+
+            if average_params:
+                target_state_dict[key].data.fill_(0.)
+
+            if average_gradients:
+                target_state_dict[key].grad = target_state_dict[key].data.clone()
+                target_state_dict[key].grad.data.fill_(0.)
+
+            for learner_id, learner in enumerate(learners):
+                state_dict = learner.model.state_dict(keep_vars=True)
+
+                if average_params:
+                    # target_state_dict[key].data += weights[learner_id] * state_dict[key].data.clone()
+                    param_val[key].append(state_dict[key].data.clone())
+
+                if average_gradients:
+                    if state_dict[key].grad is not None:
+                        # target_state_dict[key].grad += weights[learner_id] * state_dict[key].grad.clone()
+                        grad_val[key].append(state_dict[key].grad.clone())
+                    elif state_dict[key].requires_grad:
+                        warnings.warn(
+                            "trying to average_gradients before back propagation,"
+                            " you should set `average_gradients=False`."
+                        )
+
+            if average_params:
+                target_state_dict[key].data = krum_agg(param_val[key])
+                # sorted_params, _ = torch.sort(torch.stack(param_val[key], dim=0), dim=0)
+                # target_state_dict[key].data = torch.mean(sorted_params[N_removed:-N_removed], dim=0)
+            
+            if average_gradients:
+                target_state_dict[key].grad = krum_agg(grad_val[key])
+                # sorted_grads, _ = torch.sort(torch.stack(grad_val[key], dim=0), dim=0)
+                # target_state_dict[key].grad = torch.mean(sorted_grads[N_removed:-N_removed], dim=0)
+
+        else:
+            # tracked batches
+            target_state_dict[key].data.fill_(0)
+            for learner_id, learner in enumerate(learners):
+                state_dict = learner.model.state_dict()
+                target_state_dict[key].data += state_dict[key].data.clone()
 
 def partial_average(learners, average_learner, alpha):
     """
